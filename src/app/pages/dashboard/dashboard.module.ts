a5c021280c8973dc15bb9abd5e82e611444ec1e5
PuaZhiXian <xianpua2001@gmail.com>
2023-05-12 23:12:28 +0800

    testing sandbox

--- src/app/pages/dashboard/dashboard.module.ts
+++ src/app/pages/dashboard/dashboard.module.ts
@@ -3,17 +3,21 @@ import {CommonModule} from '@angular/common';
 import {DashboardComponent} from './page/dashboard/dashboard.component';
 import {DashboardRoutingModule} from "./dashboard-routing.module";
 import {WelcomeModule} from "../welcome/welcome.module";
+import {FormsModule} from "@angular/forms";
+import {NzCodeEditorModule} from "ng-zorro-antd/code-editor";
 
 
 @NgModule({
   declarations: [
     DashboardComponent
   ],
-    imports: [
-        DashboardRoutingModule,
-        CommonModule,
-        WelcomeModule
-    ],
+  imports: [
+    DashboardRoutingModule,
+    CommonModule,
+    WelcomeModule,
+    FormsModule,
+    NzCodeEditorModule
+  ],
   exports: [
     DashboardComponent
   ]
--- src/app/pages/dashboard/page/dashboard/dashboard.component.ts
+++ src/app/pages/dashboard/page/dashboard/dashboard.component.ts
@@ -1,10 +1,53 @@
-import { Component } from '@angular/core';
+import {Component, OnInit} from '@angular/core';
+import {NzConfigService} from "ng-zorro-antd/core/config";
 
 @Component({
   selector: 'app-dashboard',
   templateUrl: './dashboard.component.html',
   styleUrls: ['./dashboard.component.scss']
 })
-export class DashboardComponent {
+export class DashboardComponent implements OnInit {
+
+  constructor(private nzConfigService: NzConfigService) {
+  }
+
+  dark = true
+  programming_language = 'python'
+  loading = true
+
+  options = {
+    'python': '',
+    'java': ''
+  }
+  code =''
+
+  ngOnInit(): void {
+    const defaultEditorOption = this.nzConfigService.getConfigForComponent('codeEditor')?.defaultEditorOption || {};
+    this.nzConfigService.set('codeEditor', {
+      defaultEditorOption: {
+        ...defaultEditorOption,
+        theme: this.dark? 'vs-dark':'vs',
+        readOnly: true,
+        minimap: {enabled: false},
+      }
+    });
+    this.loading = false
+    this.code = 'sd'
+    console.log(this.options)
+  }
+
+  toggleDarkMode(){
+    this.dark = !this.dark
+    const defaultEditorOption = this.nzConfigService.getConfigForComponent('codeEditor')?.defaultEditorOption || {};
+    this.nzConfigService.set('codeEditor', {
+      defaultEditorOption: {
+        ...defaultEditorOption,
+        theme: this.dark? 'vs-dark':'vs',
+        readOnly: true,
+        minimap: {enabled: false},
+      }
+    });
+  }
+
 
 }
