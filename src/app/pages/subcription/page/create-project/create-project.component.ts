ffeb259c3aaad60ac945e414d829caa6ee48605d
PuaZhiXian <xianpua2001@gmail.com>
2023-06-09 13:09:12 +0800

    post project to db

--- src/app/pages/subcription/page/create-project/create-project.component.ts
+++ src/app/pages/subcription/page/create-project/create-project.component.ts
@@ -2,6 +2,8 @@ import {Component, OnInit} from '@angular/core';
 import {UntypedFormBuilder, UntypedFormGroup, Validators} from "@angular/forms";
 import {Router} from "@angular/router";
 import {NzMessageService} from "ng-zorro-antd/message";
+import {ProjectService} from "../../../../service/project/project.service";
+import {finalize} from "rxjs";
 
 @Component({
   selector: 'app-create-project',
@@ -13,7 +15,8 @@ export class CreateProjectComponent implements OnInit {
 
   constructor(private fb: UntypedFormBuilder,
               private router: Router,
-              private message: NzMessageService) {
+              private message: NzMessageService,
+              private projectService: ProjectService) {
   }
 
   ngOnInit(): void {
@@ -31,7 +34,13 @@ export class CreateProjectComponent implements OnInit {
   submit() {
     if (this.validateForm.valid) {
       this.message.success('Project have successfully created');
-      this.router.navigate(['/','dashboard'])
+      this.projectService.addProject(this.validateForm.value)
+        .pipe(finalize(() => {
+          this.router.navigate(['/', 'dashboard'])
+        }))
+        .subscribe((resp) => {
+
+        })
     } else {
       Object.values(this.validateForm.controls).forEach(control => {
         if (control.invalid) {
