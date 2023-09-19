802a678b2ba694787d80c74d26439bf015bf8875
PuaZhiXian <xianpua2001@gmail.com>
2023-09-19 12:22:24 +0800

    add responsive view to guide page

--- src/app/pages/guide/guide.module.ts
+++ src/app/pages/guide/guide.module.ts
@@ -4,10 +4,11 @@ import {GuideRoutingModule} from './guide-routing.module';
 import { GuideComponent } from './page/guide/guide.component';
 import {CommonModule} from "@angular/common";
 import {NzSkeletonModule} from "ng-zorro-antd/skeleton";
+import {NzCollapseModule} from "ng-zorro-antd/collapse";
 
 
 @NgModule({
-    imports: [GuideRoutingModule, CommonModule, NzSkeletonModule],
+    imports: [GuideRoutingModule, CommonModule, NzSkeletonModule, NzCollapseModule],
   declarations: [
     GuideComponent
   ],
