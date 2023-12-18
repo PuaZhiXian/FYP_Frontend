c7d72ceb1ad6044acf196e8945d3465fce210af1
Tan Zi Hao <u2005328@siswa.um.edu.my>
2023-12-19 02:43:23 +0800

    modified naming of IEnum

--- src/app/interface/api-collection/i-api-documentation-object.ts
+++ src/app/interface/api-collection/i-api-documentation-object.ts
@@ -2,11 +2,11 @@ export interface IApiDocumentationObject {
   attr_name: string,
   attr_type: string,
   attr_description: string,
-  enum?: IEnum[],
+  enum_ids?: IEnum[],
   child_attr_ids?: IApiDocumentationObject[];
 }
 
 interface IEnum {
-  name: string,
-  description: string
+  enum_name: string,
+  enum_description: string
 }
