4f89949a31f13f05fa4380cd4ec88d60ccb34c69
Tan Zi Hao <u2005328@siswa.um.edu.my>
2024-01-08 00:52:01 +0800

    change prod.ts url for deployment

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,5 +1,5 @@
 export const environment = {
   production: true,
-  apiUrl: 'http://192.168.102.118:1337',
+  apiUrl: 'https://fyp-strapi.onrender.com',
   secretKey: 'd3573701cda570c0130fcb3e8594b0d9d43113fe308abf1fe132eabe4eefc74e'
 };
