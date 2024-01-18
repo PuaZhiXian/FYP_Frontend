f7c98b3cd34cc5612f61ebe2f5c03c45bc0e61de
Tan Zi Hao <u2005328@siswa.um.edu.my>
2024-01-18 13:54:30 +0800

    added console.log

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -18,5 +18,8 @@ export class AppComponent implements OnInit{
 
   ngOnInit(): void {
     this.translate.use(this.languages[0])
+    console.log('%cMake With Love By Zi Hao & Pua', 'color: red; font-size: 24px;');
+    console.log('https://www.linkedin.com/in/tan-zi-hao-b75b40224/')
+    console.log('https://www.linkedin.com/in/zhi-xian-pua-92147b220/')
   }
 }
